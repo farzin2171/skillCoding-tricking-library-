dc4f7e35a8dd05dc9a32efd5f28f912d01ed035f
Farzin Faghirnavaz <ffaghirnavaz@safefleet.net>
2020-07-10 09:11:38 -0400

    Started Developing

---
+++ TrikingLibrary.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrikingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
